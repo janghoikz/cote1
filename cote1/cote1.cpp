bd8fa92753e01cf4140b4756b2eba02e45757b92
ohne <ghdlr785@gmail.com>
2022-06-16 10:45:19 +0900

    초기 커밋.

---
+++ cote1/cote1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int x, int n)
+{
+    vector <long long> answer{};
+    int temp;
+    for (int i = 0; i < n; i++)
+    {
+        temp = x + (x * i);
+        
+        answer.push_back(temp);
+
+        std::cout << answer[i] << std::endl;
+    }
+    
+    return answer;
+}
+
+int main()
+{
+    solution(2, 5);
+    solution(4, 3);
+    solution(-4, 2);
+}
+
